879163f827eeeb2dda4889d6aa7b70d1067c55b6
Johannes Johow <johannes.johow@gmx.de>
2015-09-22 09:49:09 +0200

    add X-chromosomal relatedness

--- R/plot_pedigree.R
+++ R/plot_pedigree.R
@@ -4,7 +4,7 @@
 #' @param id Individual ID
 #' @param evdat Dates of interest in the Year-month-day format (e.g. "1850-01-01")
 #' @param df_ind A dataframe
-#' @param ped An optional pedgree object related to x. Will be generated, if not provided.
+#' @param ped An optional pedgree object related to id. Will be generated, if not provided.
 #' @param evmat Event matrix
 #' @param label optional vector of labels to add to pedigree members
 #' @param cex size of text and symbols
@@ -22,7 +22,7 @@ plot_pedigree <- function(id, evdat, df_ind, ped=NULL, evmat, label = NULL, cex=
   }
  if(is.null(label)) {  label <-  ifelse(is_alive(ped$id, paste(evdat), df_ind)==1,
     paste(ifelse(ped$id %in% as.numeric(dimnames(evmat)$id), paste0(
-                round(get_age(ifelse(ped$id %in% as.numeric(dimnames(evmat)$id), ped$id, x), paste(evdat), evmat=evmat)), "y"), "?"),
+                round(get_age(ifelse(ped$id %in% as.numeric(dimnames(evmat)$id), ped$id, id), paste(evdat), evmat=evmat)), "y"), "?"),
           gsub("NA", "?", recode_evloc(na2zero(unlist(lapply(ped$id[which(ped$id %in% dimnames(evmat)$id)], get_kinloc, paste(evdat), evmat = evmat)), zero=34))),
           sep="\n"), "")}
   kinship2::plot.pedigree(ped, affected = is_alive(ped$id, paste(evdat), df_ind),
