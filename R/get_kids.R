23ea24d8a2d5ed961396275c1668987c483d6726
Johannes Johow <johannes.johow@gmx.de>
2017-02-14 13:19:42 +0100

    Update get_kids.R
    Add grandchildren + later descendents

--- R/get_kids.R
+++ R/get_kids.R
@@ -3,6 +3,7 @@
 #' This function returns the ID(s) of an individual's children
 #' @param x An individual ID
 #' @param df_ped A dataframe
+#' @param include_all Should offspring in subsequent generations (grandchildren etc.) be included?
 #' @keywords kh children
 #' @export
 #' @examples
@@ -10,7 +11,19 @@
 #' get_kids(df_ped$id[2], df_ped)
 #' detach(kh33::kh)
 #' }
-get_kids <- function(x, df_ped){
+get_kids <- function(x, df_ped, include_all = FALSE){
   x <- x[which(x > 0 & ( x %in% df_ped$momid | x %in% df_ped$dadid))]
-    return(unique(df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x]))
+  if (include_all==TRUE){
+   
+    out_x <- unique(c(df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x], 
+                      df_ped$id[df_ped$momid %in% df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x] |
+                                df_ped$dadid %in% df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x]],
+                      df_ped$id[df_ped$momid %in% df_ped$id[df_ped$momid %in% df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x] |
+                                                              df_ped$dadid %in% df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x]] |
+                                  df_ped$dadid %in% df_ped$id[df_ped$momid %in% df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x] |
+                                                                df_ped$dadid %in% df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x]]]))
+  } else {
+  out_x <- unique(df_ped$id[df_ped$momid %in% x | df_ped$dadid %in% x])
+  }
+    return(out_x)
 }
